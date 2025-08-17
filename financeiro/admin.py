4544b678add662d6f42d0a9c71b9ef5c58b8c4a8
igor <igor.rodrigues@livon.io>
2025-08-17 18:07:53 -0300

    Enhance admin interface with import/export functionality and improve dashboard styling

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -1,1 +1,52 @@
-                                       
+from django.contrib import admin
+from .models import Entrada, Saida, ContaPagar, ContaReceber
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+# Resources
+class EntradaResource(resources.ModelResource):
+    class Meta:
+        model = Entrada
+
+class SaidaResource(resources.ModelResource):
+    class Meta:
+        model = Saida
+
+class ContaPagarResource(resources.ModelResource):
+    class Meta:
+        model = ContaPagar
+        fields = ('id', 'descricao','documento', 'valor', 'data_vencimento', 'categoria', 'pago', 'criado_em', 'atualizado_em')
+
+class ContaReceberResource(resources.ModelResource):
+    class Meta:
+        model = ContaReceber
+        fields = ('id', 'descricao','documento', 'valor', 'data_vencimento', 'categoria', 'recebido', 'criado_em', 'atualizado_em')
+
+# Admins
+@admin.register(Entrada)
+class EntradaAdmin(ImportExportModelAdmin):
+    resource_class = EntradaResource
+    list_display = ('descricao', 'valor', 'data', 'categoria', 'recebido_por', 'criado_em', 'atualizado_em')
+    search_fields = ('descricao', 'categoria')
+    list_filter = ('data', 'categoria')
+
+@admin.register(Saida)
+class SaidaAdmin(ImportExportModelAdmin):
+    resource_class = SaidaResource
+    list_display = ('descricao', 'valor', 'data', 'categoria', 'pago_por', 'criado_em', 'atualizado_em')
+    search_fields = ('descricao', 'categoria')
+    list_filter = ('data', 'categoria')
+
+@admin.register(ContaPagar)
+class ContaPagarAdmin(ImportExportModelAdmin):
+    resource_class = ContaPagarResource
+    list_display = ('descricao','documento', 'valor', 'data_vencimento', 'categoria', 'pago', 'criado_em', 'atualizado_em')
+    search_fields = ('descricao', 'categoria','documento')
+    list_filter = ('data_vencimento', 'categoria', 'pago')
+
+@admin.register(ContaReceber)
+class ContaReceberAdmin(ImportExportModelAdmin):
+    resource_class = ContaReceberResource
+    list_display = ('descricao','documento', 'valor', 'data_vencimento', 'categoria', 'recebido', 'criado_em', 'atualizado_em')
+    search_fields = ('descricao', 'categoria','documento')
+    list_filter = ('data_vencimento', 'categoria', 'recebido')
--- oatelier_django/urls.py
+++ oatelier_django/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 from oatelier_django.views import dashboard
 from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', dashboard, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('gerador/', admin.site.admin_view(lambda request: render(request, 'gerador.html')), name='gerador'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('dashboard/', login_required(dashboard), name='dashboard'),
+    path('gerador/', TemplateView.as_view(template_name='gerador.html'), name='gerador'),
+]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
