bde5fa54b244469ce2a630a1df61c467e38a162f
Igor <igor.rodrigues@livon.io>
2025-08-24 22:23:26 -0300

    Refactor settings.py for improved readability and maintainability; update allowed hosts for testing

--- oatelier_django/settings.py
+++ oatelier_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.100.11', '192.168.0.30','oatelier.mrsolutions.dev.br' , 'localhost', '193.203.175.175', 'staging-gold.onrender.com']
+ALLOWED_HOSTS = ['*']  # Para testes, aceita conexões de qualquer host. Em produção, especifique os IPs/domínios permitidos.
 
 
 # Application definition
